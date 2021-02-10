ee970b28cfd220e56e0c3a38530b408de3ec3492
NightFeather <reingeass@gmail.com>
2021-02-10 18:09:36 +0800

    Create VariablesAndFunctions.cs

---
+++ UnityCsharp/CommUnity/VariablesAndFunctions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+    int myInt = 5; //int = integer
+
+    private void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+    int MultiplyByTwo(int number)
+    {
+        int result;
+        result = number * 3;
+        return result; //返回數值 記得留空間存放
+    }
+}
