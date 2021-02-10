79a880e036bbe103565d22ed3ac05bdd36510e74
NightFeather <reingeass@gmail.com>
2021-02-10 18:09:31 +0800

    Create IfStatements.cs

---
+++ UnityCsharp/CommUnity/IfStatements.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+    public float coffeeTemperature = 85.0f;
+    float hotLimitTemperature = 70.0f;
+    float coldLimitTemperature = 40.0f;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            TemperatureTest();
+
+            coffeeTemperature -= Time.deltaTime * 5f;
+        }
+    }
+
+    void TemperatureTest()
+    {
+        if (coffeeTemperature > hotLimitTemperature)
+        {
+            print("Coffee is too hot.");
+        }
+        else if (coffeeTemperature < coldLimitTemperature)
+        {
+            print("Coffee is too cold.");
+        }
+        else
+        {
+            print("Coffee is just right");
+        }
+    }
+}
