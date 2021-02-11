849033ffbc19302e6203b9d32fdd0235bf278bcc
NightFeather <reingeass@gmail.com>
2021-02-11 19:38:44 +0800

    Update CommUnityNote.cs

--- UnityCsharp/CommUnity/CommUnityNote.cs
+++ UnityCsharp/CommUnity/CommUnityNote.cs
@@ -45,22 +45,22 @@ public class CommUnityNote : MonoBehaviour
 
 /*
  01 行為組件腳本
- 02 變數和函數       ExampleBehaviourScript
- 03 約定和語法       VariablesAndFunctions
- 04 IF 語句         IfStatements
+ 02 變數和函數        ExampleBehaviourScript
+ 03 約定和語法        VariablesAndFunctions
+ 04 IF 語句          IfStatements
  05 For Loop*
- 06 作用域和訪問修飾符ScopeAndAccessModifiers
+ 06 作用域和訪問修飾符 ScopeAndAccessModifiers
  07 Awake & Start   AwakeAndStart
  08 Update & Fixed* UpdateAndFixedUpdate
- 09
- 10
- 11
- 12
- 13
- 14
- 15
- 16
- 17
+ 09 矢量數學         Vector3.magnitude Vector3.Dot(VectorA, VectorB)點積計算 Vector3.Cross(VectorA, VectorB)叉積計算
+ 10 啟用和禁用組件    EnableComponents
+ 11 激活遊戲對象      ActiveObject
+ 12 平移和旋轉       Translate Rotate
+ 13 Look at         CameraLookAt
+ 14 -
+ 15 Destroy         DestroyBasic
+ 16 GetButton GetKey 返回傳的是 Bool
+ 17 GetAxis*         傳回的則是 float -1~1
  18
  19
  20
