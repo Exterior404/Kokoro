8560b91a77b12f2a97e73a6a28b4464494a6a830
NightFeather <reingeass@gmail.com>
2021-02-11 19:38:36 +0800

    Create ActiveObject.cs

---
+++ UnityCsharp/CommUnity/ActiveObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActiveObject : MonoBehaviour
+{
+    public GameObject myGameObject;
+
+    void Start()
+    {
+        Debug.Log("Active Self: /" + myGameObject);
+        Debug.Log("Active in Hierarchy: /" + myGameObject.activeInHierarchy);
+        gameObject.SetActive(false);//關父對象
+    }
+
+    void Update()
+    {
+
+    }
+}
