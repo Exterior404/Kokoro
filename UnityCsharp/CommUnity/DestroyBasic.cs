567b96a9763031474f0888e617b88db97677f5f9
NightFeather <reingeass@gmail.com>
2021-02-11 19:38:57 +0800

    Create DestroyBasic.cs

---
+++ UnityCsharp/CommUnity/DestroyBasic.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyBasic : MonoBehaviour
+{
+    public GameObject other;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {//會刪除對象 (自己)
+            Destroy(gameObject);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {//刪別的對象 需掛載
+            Destroy(other);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {//刪除組件的方式 以及延時刪除
+            Destroy(GetComponent<Light>(), 6f);
+        }
+    }
+}
