0cc3cdb5f6c94ac54692aea5fd446f2634d9f60b
NightFeather <reingeass@gmail.com>
2021-02-11 19:38:46 +0800

    Create CameraLookAt.cs

---
+++ UnityCsharp/CommUnity/CameraLookAt.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraLookAt : MonoBehaviour
+{//攝影機追隨物體
+    public Transform target;
+
+    void Update()
+    {
+        transform.LookAt(target);
+    }
+}
