830ef4b0de8655ed0e51c2fecd55bacd1c42093a
NightFeather <reingeass@gmail.com>
2021-02-12 21:55:30 +0800

    Create LoopDoFor.cs

---
+++ UnityCsharp/CommUnity/LoopDoFor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoopDoFor : MonoBehaviour
+{//Note
+    void Start()
+    {
+        int cupInTheSink = 4;
+
+        void LoopWhile()
+        {
+            while (cupInTheSink > 0)
+            {//持續判斷
+                Debug.Log("I've washed a cup!");
+                cupInTheSink--;
+            }
+        }
+
+        void LoopDoWhile()
+        {
+            bool shouldContinue = false;
+
+            do
+            { //先執行這邊的內容 才進入判斷條件
+                print("Hello World");
+            } while (shouldContinue == true);// <- 後做判斷 ;
+        }
+
+        void LoopFor()
+        {
+            int numEnemies = 3;
+
+            for (int i = 0; i < numEnemies; i++)
+            {//變量 條件 迭代值
+                Debug.Log("Creating enemy number: " + i);
+            }
+        }
+    }
+}
