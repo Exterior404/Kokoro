2703eae72ea9ac119801d03210b130c38a459825
NightFeather <reingeass@gmail.com>
2021-02-12 21:55:26 +0800

    Update CommUnityNote.cs

--- UnityCsharp/CommUnity/CommUnityNote.cs
+++ UnityCsharp/CommUnity/CommUnityNote.cs
@@ -1,54 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CommUnityNote : MonoBehaviour
-{
-    #region Loop
-    int cupInTheSink = 4;
-
-    void LoopWhile()
-    {
-        while (cupInTheSink > 0)
-        {//持續判斷
-            Debug.Log("I've washed a cup!");
-            cupInTheSink--;
-        }
-    }
-
-    void LoopDoWhile()
-    {
-        bool shouldContinue = false;
-
-        do
-        { //先執行這邊的內容 才進入判斷條件
-            print("Hello World");
-        } while (shouldContinue == true);// <- 後做判斷 ;
-    }
-
-    void LoopFor()
-    {
-        int numEnemies = 3;
-
-        for (int i = 0; i < numEnemies; i++)
-        {//變量 條件 迭代值
-            Debug.Log("Creating enemy number: " + i);
-        }
-    }
-    #endregion
-
-    private void Start()
-    {
-
-    }
-}
-
-/*
+﻿/*  C#初級教程
  01 行為組件腳本
  02 變數和函數        ExampleBehaviourScript
  03 約定和語法        VariablesAndFunctions
  04 IF 語句          IfStatements
- 05 For Loop*
+ 05 For Loop        LoopDoFor
  06 作用域和訪問修飾符 ScopeAndAccessModifiers
  07 Awake & Start   AwakeAndStart
  08 Update & Fixed* UpdateAndFixedUpdate
@@ -71,4 +26,8 @@ public class CommUnityNote : MonoBehaviour
  25
  26
  27
-     */
+ */
+
+/*  C#中級教程
+ 01
+ */
