3426e3aa877c79fb5fb406e07fa5dcc9e97a533d
NightFeather <reingeass@gmail.com>
2021-02-15 17:26:41 +0800

    Create PrefabDesScript.cs

---
+++ UnityCsharp/CommUnity/PrefabDesScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabDesScript : MonoBehaviour
+{//射擊物自毀用 默認一秒
+    public float DesSec = 1.0f;
+
+    void Start()
+    {
+        Destroy(gameObject, DesSec);
+    }
+}
