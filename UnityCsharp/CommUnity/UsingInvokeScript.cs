6e7bcf312ebf55c38cc308d149f2e8dfa582af6a
NightFeather <reingeass@gmail.com>
2021-02-17 22:42:01 +0800

    Create UsingInvokeScript.cs

---
+++ UnityCsharp/CommUnity/UsingInvokeScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UsingInvokeScript : MonoBehaviour
+{
+    public GameObject target;
+
+    void Start()
+    {
+        Invoke("SpawnObject", 2);                
+    }
+
+    void SpawnObject()
+    {
+        Instantiate(target, new Vector3(0, 3, 0), Quaternion.identity);
+    }
+}
