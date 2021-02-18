5f6681eb28dc67dc846dc55ab20e2f988ffe2e0c
NightFeather <reingeass@gmail.com>
2021-02-18 20:11:10 +0800

    Create CsharpNote.cs

---
+++ UnityCsharp/CodeNote/Csharp8/CsharpNote.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public partial class CsharpNote : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log(abc);
+    }
+}
