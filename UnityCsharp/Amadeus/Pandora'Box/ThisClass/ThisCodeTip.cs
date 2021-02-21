da9606d1750bdd5a9fc82efcdc9fd8aed92da47a
NightFeather <reingeass@gmail.com>
2021-02-22 07:31:29 +0800

    Create ThisCodeTip.cs

---
+++ UnityCsharp/Amadeus/Pandora'Box/ThisClass/ThisCodeTip.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public static class ThisCodeTip
+{
+    public static void DoSomething(this GameObject obj)
+    {//製做一個公開的靜態 this會指向這份文件
+        Debug.Log("使用了擴展文件");
+    }
+
+    public static string ToHex(this Color color)
+    {//將RGB代碼 轉換成 Html的Hex碼 就是 #FFFFFF
+        return ColorUtility.ToHtmlStringRGB(color);
+    }
+}
