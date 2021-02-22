5b07b83d9703e6308d06b1ff5c32577be367a7d7
NightFeather <reingeass@gmail.com>
2021-02-22 17:57:32 +0800

    Update LoopDoFor.cs

--- UnityCsharp/CommUnity/LoopDoFor.cs
+++ UnityCsharp/CommUnity/LoopDoFor.cs
@@ -3,37 +3,41 @@ using System.Collections;
 
 public class LoopDoFor : MonoBehaviour
 {//Note
+    public int cupInTheSink = 4;
+
     void Start()
     {
-        int cupInTheSink = 4;
+        //LoopWhile();
+        //LoopDoWhile();
+        //LoopFor();
+    }
 
-        void LoopWhile()
-        {
-            while (cupInTheSink > 0)
-            {//持續判斷
-                Debug.Log("I've washed a cup!");
-                cupInTheSink--;
-            }
+    void LoopWhile()
+    {
+        while (cupInTheSink > 0)
+        {//持續判斷
+            Debug.Log("I've washed a cup!");
+            cupInTheSink--;
         }
+    }
 
-        void LoopDoWhile()
-        {
-            bool shouldContinue = false;
+    void LoopDoWhile()
+    {
+        bool shouldContinue = false;
 
-            do
-            { //先執行這邊的內容 才進入判斷條件
-                print("Hello World");
-            } while (shouldContinue == true);// <- 後做判斷 ;
-        }
+        do
+        { //先執行這邊的內容 才進入判斷條件
+            print("Hello World");
+        } while (shouldContinue == true);// <- 後做判斷 ;
+    }
 
-        void LoopFor()
-        {
-            int numEnemies = 3;
+    void LoopFor()
+    {
+        int numEnemies = 3;
 
-            for (int i = 0; i < numEnemies; i++)
-            {//變量 條件 迭代值
-                Debug.Log("Creating enemy number: " + i);
-            }
+        for (int i = 0; i < numEnemies; i++)
+        {//變量 條件 迭代值
+            Debug.Log("Creating enemy number: " + i);
         }
     }
 }
