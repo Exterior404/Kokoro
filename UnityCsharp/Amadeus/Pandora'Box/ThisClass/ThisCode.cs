4fe55731da457f71069bfc724e481c01c0a19450
NightFeather <reingeass@gmail.com>
2021-02-23 16:24:32 +0800

    Create ThisCode.cs

---
+++ UnityCsharp/Amadeus/Pandora'Box/ThisClass/ThisCode.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThisCode : MonoBehaviour
+{//轉換顏色代碼
+    public GameObject obj;
+
+    public Text title;
+    public Color color;
+
+    private void Start()
+    {
+        Debug.Log("準備使用擴展");
+        obj.DoSomething();
+
+        title.text = string.Format("<#{0}>{1}</color>",color.ToHex(),"Welcome Hell");
+    }
+}
