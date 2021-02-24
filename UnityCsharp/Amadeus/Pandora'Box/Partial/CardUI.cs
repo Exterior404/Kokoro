ceb9d20125f5bb0d0fca836606c8179c6343c255
NightFeather <reingeass@gmail.com>
2021-02-24 17:53:19 +0800

    Create CardUI.cs

---
+++ UnityCsharp/Amadeus/Pandora'Box/Partial/CardUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class CardUI : MonoBehaviour
+{
+    void Start()
+    {
+        Post01();
+        Post02();
+        Post03();
+    }
+
+    void Update()
+    {
+        
+    }
+}
+
+
+/*
+    這份是主要控制文件 Class 名還是要一致 
+    其他散文 的Class 也要跟這邊一樣 
+    然後不需要 :MonoBehaviour
+    將主文件 掛上 GameObject就可以了
+*/
