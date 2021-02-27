6917ea7bb360e48c1d245784a834c1cefe02d1f0
NightFeather <reingeass@gmail.com>
2021-02-28 01:04:05 +0800

    Create UtilitiesExample.cs

---
+++ UnityCsharp/CommUnityEx/useStatic/UtilitiesExample.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UtilitiesExample : MonoBehaviour
+{
+    private void Start()
+    {
+        int x = Utilities.Add(5, 6);
+        Debug.Log("Ex: " + x);
+    }
+}
