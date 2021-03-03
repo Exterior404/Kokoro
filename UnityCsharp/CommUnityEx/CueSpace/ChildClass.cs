65eabce87e6aa4eefcc568e55690dc331f317852
NightFeather <reingeass@gmail.com>
2021-03-04 00:36:19 +0800

    Create ChildClass.cs

---
+++ UnityCsharp/CommUnityEx/CueSpace/ChildClass.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CUE {
+    public class ChildClass : CUE.ParentClass {
+
+        float SomeValue = 5.0f;
+
+        void Start()
+        {
+            Debug.Log(SomeValue);
+        }
+
+        void SayHello()
+        {
+            
+            // Do it
+        }
+    }
+}
