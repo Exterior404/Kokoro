cd1a1117689ef164d1e38cd58fd745862bf77cb8
NightFeather <reingeass@gmail.com>
2021-03-20 07:38:45 +0800

    Create TeleportScript.cs

---
+++ UnityCsharp/CommUnityEx/TeleportScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportScript : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        CUEvent.OnClicked += Teleport;
+    }
+
+    private void OnDisable()
+    {
+        CUEvent.OnClicked -= Teleport;
+    }
+
+    void Teleport()
+    {
+        Vector3 pos = transform.position;
+        pos.y = Random.Range(0.3f, 1.0f);
+        transform.position = pos;
+    }
+}
