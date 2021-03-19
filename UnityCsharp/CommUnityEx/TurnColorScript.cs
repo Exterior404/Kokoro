7b112bc2da74df2b7e907dcf2737ea45ab55fba6
NightFeather <reingeass@gmail.com>
2021-03-20 07:38:51 +0800

    Create TurnColorScript.cs

---
+++ UnityCsharp/CommUnityEx/TurnColorScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnColorScript : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        CUEvent.OnClicked += TurnColor;
+    }
+
+    private void OnDisable()
+    {
+        CUEvent.OnClicked -= TurnColor;
+    }
+
+    void TurnColor()
+    {
+        Color col = new Color(Random.value, Random.value, Random.value);
+        GetComponent<Renderer>().material.color = col;
+    }
+}
