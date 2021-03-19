1efe2f3d94d1304a277fa815cc0bedebe18912f3
NightFeather <reingeass@gmail.com>
2021-03-19 18:14:51 +0800

    Create Motion.cs

---
+++ UnityCsharp/CommUnityEx/LockAtEuler/Motion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motion : MonoBehaviour
+{
+    public float speed = 3f;
+
+    void Start()
+    {
+        
+    }
+
+
+    void FixedUpdate()
+    {
+        transform.Translate(-Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);   
+    }
+}
