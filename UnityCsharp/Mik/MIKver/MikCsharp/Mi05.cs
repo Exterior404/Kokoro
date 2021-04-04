6391c4f12a1a85a9d66b2f7868c8740b5ca78266
NightFeather <reingeass@gmail.com>
2021-04-04 14:58:48 +0800

    Create Mi05.cs

---
+++ UnityCsharp/Mik/MIKver/MikCsharp/Mi05.cs
@@ -0,0 +1,52 @@
+﻿using EarthP;
+using MarsP;
+
+namespace EarthP
+{
+    public class EarthPeople {
+        int manAge = 30;
+        int womanAge = 25;
+        float manHeight = 175.3f;
+        float womanHeight = 165.3f;
+        string manName = "Curry";
+        string womanName = "Marry";
+    }
+}
+
+namespace MarsP
+{
+    public class MarsPeople
+    {
+        int manAge = 30;
+        int womanAge = 25;
+        float manHeight = 175.3f;
+        float womanHeight = 165.3f;
+        string manName = "Curry";
+        string womanName = "Marry";
+    }
+}
+
+
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// 命名空間
+
+namespace MikNote
+{//命名空間的名稱= 腳本
+    public class Mi05 : MonoBehaviour
+    {//類別與腳本名稱
+
+        void Start()
+        {//方法
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
+*/
