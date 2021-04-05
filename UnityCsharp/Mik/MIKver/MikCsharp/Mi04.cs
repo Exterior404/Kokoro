cf6796a03f1659fd3029330e1623a2a46406fad5
NightFeather <reingeass@gmail.com>
2021-04-05 20:17:43 +0800

    Create Mi04.cs

---
+++ UnityCsharp/Mik/MIKver/MikCsharp/Mi04.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mi04 : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    public class People {//類別 
+        int manAge = 30; //屬性
+        int womanAge = 25;
+        float manHeight = 175.3f;
+        float womanHeight = 165.3f;
+        string manName = "Curry";
+        string womanName = "Marry";
+
+    }
+}
