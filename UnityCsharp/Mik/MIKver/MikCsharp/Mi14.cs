f7ecf1e776c20cbcaf0aa914c7e56c62c7f05cd2
NightFeather <reingeass@gmail.com>
2021-04-16 17:36:30 +0800

    Create Mi14.cs

---
+++ UnityCsharp/Mik/MIKver/MikCsharp/Mi14.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mi14 : MonoBehaviour
+{
+    public Text numberText;
+    public byte number = 255;
+
+    void Start()
+    {
+        numberText.text = number.ToString();
+        
+    }
+}
