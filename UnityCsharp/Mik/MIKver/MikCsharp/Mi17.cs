a583e05d91059c2db844df44e1c15d730cb200d2
NightFeather <reingeass@gmail.com>
2021-04-18 23:48:58 +0800

    Create Mi17.cs

---
+++ UnityCsharp/Mik/MIKver/MikCsharp/Mi17.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mi17 : MonoBehaviour
+{//三元表達式
+    bool isTrue;
+    int result;
+    int x = 100, y = 200;
+
+    void Start()
+    {
+        result = x > y ? x : y;
+
+        Debug.Log(result);//200
+    }
+}
