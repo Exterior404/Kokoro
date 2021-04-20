0df433a73b62cc361fca607c4e1a584762d1bb8c
NightFeather <reingeass@gmail.com>
2021-04-20 18:56:23 +0800

    Create Dog.cs

---
+++ UnityCsharp/Mik/MIKver/MikCsharp/Dog.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mik {
+    public class Dog {
+        public int dogAge;
+        public float dogHeight;
+        public float dogWeight;
+
+        public Dog() {
+            this.dogAge = 10;
+            this.dogHeight = 100;
+            this.dogWeight = 15;
+
+            Debug.Log("Dog的建構方法被呼叫了");
+        }
+    }
+}
