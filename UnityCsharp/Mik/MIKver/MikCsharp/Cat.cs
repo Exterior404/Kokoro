42fde48b28edee3a443639871afdc3600fad9395
NightFeather <reingeass@gmail.com>
2021-04-21 20:11:47 +0800

    Create Cat.cs

---
+++ UnityCsharp/Mik/MIKver/MikCsharp/Cat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mik {
+    public class Cat : MonoBehaviour {
+        public int catAge;
+        public float catHeight;
+        public float catWeight;
+
+        public Cat() {
+            this.catAge = 25;
+            this.catHeight = 75;
+            this.catWeight = 5;
+            Debug.Log("Cat的建構方法被呼叫了");
+        }
+    }
+}
