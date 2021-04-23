486af9420e96867ca8df6da7b9b57d6a4475ae22
NightFeather <reingeass@gmail.com>
2021-04-23 23:36:55 +0800

    Create Mi21.cs

---
+++ UnityCsharp/Mik/MIKver/MikCsharp/Mi21.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mi21 : MonoBehaviour
+{//取得對象的組件來操作
+    Mik.Cat cat;
+
+    void Start() {
+        cat = GameObject.Find("MikObject").GetComponent<Mik.Cat>();
+        cat.catAge = 123;
+    }// 有多層級就 上/中/下
+}
