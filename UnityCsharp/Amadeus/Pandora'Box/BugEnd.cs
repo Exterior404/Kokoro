9a3d0889a13190a79a6137d9ace96383100238fc
NightFeather <reingeass@gmail.com>
2021-05-02 17:27:48 +0800

    Create BugEnd.cs

---
+++ UnityCsharp/Amadeus/Pandora'Box/BugEnd.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugEnd : MonoBehaviour {
+    private void Start() {
+        ;;;;;;//不會有事
+        //Ex01();
+    }
+
+    void Ex01() {
+        for(; ; ) {
+            Debug.Log("可以運行，但會當機");
+        }
+    }
+}
