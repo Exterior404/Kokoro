e745224b6e6f737c8a36ce7e3190724c0ef8198a
NightFeather <reingeass@gmail.com>
2021-05-17 00:07:13 +0800

    Create DelgateCS.cs

---
+++ UnityCsharp/NightFeather/CS/DelgateCS.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NF {
+    public class DelgateCS : MonoBehaviour {
+
+        public delegate void XD1();
+        public XD1 myDelegate1;//默認null
+        public XD1 myDelegate2;
+        public XD1 myDelegate3;
+
+        void Start() {
+
+            myDelegate1 = delegate () { Debug.Log("HI"); };
+            myDelegate1();
+
+            //-------------
+
+            myDelegate2 = delegate () { Debug.Log("A"); };
+            myDelegate3 = delegate () { Debug.Log("B"); };
+
+            myDelegate1 = myDelegate2 + myDelegate3;
+            myDelegate1();
+
+            //-------------
+
+            myDelegate2 = null;
+            myDelegate3 = Meth01;
+            myDelegate1 = Meth02;
+
+            myDelegate1 += myDelegate2 + myDelegate3;
+            myDelegate1();
+        }
+
+        void Meth01() {
+            Debug.Log("ABC");
+        }
+
+        void Meth02() {
+            Debug.Log("DEF");
+        }
+    }
+}
