c375129c1843304d94361ce90bac26f1e3769d80
NightFeather <reingeass@gmail.com>
2021-05-25 20:21:52 +0800

    Create NFPhotonRealtimeCallBack.cs

---
+++ UnityPhoton/NF/Day01/NFPhotonRealtimeCallBack.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
+
+namespace NF.Day01 {
+    /// <summary>
+    /// Photon.Realtime全部回呼函式(CallBack)
+    /// </summary>
+    public partial class NFPhotonRealtimeCallBack : MonoBehaviourPunCallbacks {
+        private void Awake() {
+            DontDestroyOnLoad(this); //加載新場景勿 清理此物件
+        }
+    }
+}
