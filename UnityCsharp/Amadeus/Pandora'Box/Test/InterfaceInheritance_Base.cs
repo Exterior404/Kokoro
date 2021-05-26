63c3a04fd8534faac539d60fc5f83934026a0693
NightFeather <reingeass@gmail.com>
2021-05-26 18:36:14 +0800

    Create InterfaceInheritance_Base.cs

---
+++ UnityCsharp/Amadeus/Pandora'Box/Test/InterfaceInheritance_Base.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NF.Test
+{
+    public class InterfaceInheritance_Base : MonoBehaviour
+    {
+        public virtual void Base01()
+        {
+            Debug.Log("Base");
+        }
+    }
+}
