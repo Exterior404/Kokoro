0ee5bf413a02c378fec378c78cc160d07d71ee1f
NightFeather <reingeass@gmail.com>
2021-05-26 18:35:29 +0800

    Create InterfaceInheritance_Over.cs

---
+++ UnityCsharp/Amadeus/Pandora'Box/Test/InterfaceInheritance_Over.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NF.Test
+{
+    public partial class InterfaceInheritance_Par : InterfaceInheritance_Base
+    {
+        public override void Base01()
+        {
+            base.Base01();
+            Debug.Log("Over");
+        }
+
+        void Start()
+        {
+            InterfaceInheritance_Par stPar = new InterfaceInheritance_Par();
+            stPar.Halo();
+
+            Base01();
+            Halo();
+            Vector(5, 6);
+
+            stPar.Y = 4;
+            Debug.Log(stPar.Y);
+        }
+    }
+}
