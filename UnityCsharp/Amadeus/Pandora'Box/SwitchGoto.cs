329831024061b8abb907300649d9ef1bbcb41c25
NightFeather <reingeass@gmail.com>
2021-05-27 12:12:14 +0800

    Update SwitchGoto.cs

--- UnityCsharp/Amadeus/Pandora'Box/SwitchGoto.cs
+++ UnityCsharp/Amadeus/Pandora'Box/SwitchGoto.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class SwitchGoto : MonoBehaviour
 {
     public int a = 0;
-    void Start()
-    {
+    public 點菜啦 _Menu = 點菜啦.待點;
+
+    void Start() {
+        Methed01();
+        Methed02();
+    }
+
+    void Methed01() {
     sw3:
         switch (a)
         {
@@ -32,4 +38,50 @@ public class SwitchGoto : MonoBehaviour
         if (a == 4)
             Debug.Log("跳轉到這");
     }
+
+    void Methed02() {
+
+        switch (_Menu)
+        {
+            case 點菜啦.待點: Methed00();break;
+            case 點菜啦.A餐單點:
+                Methed01();
+                break;
+            case 點菜啦.A餐加飲料:
+                Methed01(); Methed02();
+                break;
+            case 點菜啦.B餐單點:
+                Methed03();
+                break;
+            case 點菜啦.B餐加飲料:
+                Methed03(); Methed04();
+                break;
+            case 點菜啦.單點飲料:
+                Methed05();
+                break;
+            default:
+                Debug.Log("沒人來就擦桌子");
+                break;
+        }
+
+        void Methed00() {
+            Debug.Log("等待中");
+        }
+
+        void Methed01() { Debug.Log("A餐內容"); }
+        void Methed02() { Debug.Log("A餐飲料"); }
+        void Methed03() { Debug.Log("B餐內容"); }
+        void Methed04() { Debug.Log("B餐飲料"); }
+        void Methed05() { Debug.Log("單點飲料"); }
+    }
+
+    public enum 點菜啦 : byte 
+    {
+        待點,
+        A餐單點,
+        A餐加飲料,
+        B餐單點,
+        B餐加飲料,
+        單點飲料
+    };
 }
