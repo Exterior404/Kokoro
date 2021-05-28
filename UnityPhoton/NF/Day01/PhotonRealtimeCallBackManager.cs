00eaaaebcfc2489bd816fb98a121b0568f21b02e
NightFeather <reingeass@gmail.com>
2021-05-29 05:45:27 +0800

    Create PhotonRealtimeCallBackManager.cs

---
+++ UnityPhoton/NF/Day01/PhotonRealtimeCallBackManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
+using NF.Day01;
+
+namespace NF.Day01 {
+    public class PhotonRealtimeCallBackManager : NFPhotonRealtimeCallBack
+    {
+        void Start() { //連線設定
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+        public override void OnConnected()
+        {
+            base.OnConnected();
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            base.OnConnectedToMaster();
+            BtnManager.inst.objMaster.SetActive(true);
+        }
+
+        public override void OnJoinedLobby()
+        {
+            base.OnJoinedLobby();
+            BtnManager.inst.objLobby.SetActive(true);
+        }
+
+        //-
+
+        public override void OnLeftLobby()
+        {
+            base.OnLeftLobby();
+            BtnManager.inst.objMaster.SetActive(true);
+        }
+
+        public override void OnJoinedRoom()
+        {
+            base.OnJoinedRoom();
+            BtnManager.inst.objRoom.SetActive(true);
+        }
+
+        public override void OnCreateRoomFailed(short returnCode, string message)
+        {
+            base.OnCreateRoomFailed(returnCode, message);
+            BtnManager.inst.objLobby.SetActive(true);
+        }
+
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            base.OnJoinRandomFailed(returnCode, message);
+            BtnManager.inst.objLobby.SetActive(true);
+        }
+
+        public override void OnJoinRoomFailed(short returnCode, string message)
+        {
+            base.OnJoinRoomFailed(returnCode, message);
+            BtnManager.inst.objLobby.SetActive(true);
+        }
+    }
+
+}
