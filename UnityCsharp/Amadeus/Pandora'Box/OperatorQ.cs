7689c44362654c8b1c937cc6692d72314ba3efc7
NightFeather <reingeass@gmail.com>
2021-06-02 18:06:35 +0800

    Create OperatorQ.cs

---
+++ UnityCsharp/Amadeus/Pandora'Box/OperatorQ.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OperatorQ : MonoBehaviour
+{
+    public int testA = 5;
+    public int testB = 10;
+
+    delegate void MyDelegate();
+    MyDelegate delgatA;
+    MyDelegate delgatB;
+
+    void Start()
+    {
+        Methed01();
+        Methed02();
+    }
+
+    void Methed01()
+    {// ?; 的使用方式
+        string strA = testA > testB ? "A大" : "A小";
+        print(strA);
+
+        int intA = testA > testB ? 1 : 0;
+
+        //這段委派不能使用 執行會報錯
+        //delgatA = intA > 0 ? MethedA : MethedB;
+        //delgatA();//空的不能用
+
+        if (intA > 0)
+        {//委派不能使用 ?:
+            delgatA = MethedA;
+            delgatA();
+        }
+        else
+        {
+            delgatA = MethedB;
+            delgatA();
+        }
+        // 判斷對錯 ? 選項是 : 選項否
+    }
+
+    void Methed02()
+    {// ?? 問當前的數值是不是 空的null
+        byte? byeA = null;
+        int a = byeA ?? -1;
+        Debug.Log(a);
+
+        byte? byeB = 1;
+        int b = byeB ?? 2;
+        Debug.Log(b);
+
+        delgatB = delgatB ?? delgatA;
+        delgatB();
+
+        /* ??         
+        是就給值
+        不是就給空
+
+        屬於當前不確定的用法 難怪會表示黑人問號
+         */
+    }
+
+    void MethedA()
+    {
+        print("可以");
+    }
+
+    void MethedB()
+    {
+        Debug.Log("不能");
+    }
+}
