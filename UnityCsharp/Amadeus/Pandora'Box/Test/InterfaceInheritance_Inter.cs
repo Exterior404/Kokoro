a6927a1ea9d70718564ec41d57783b1136113999
NightFeather <reingeass@gmail.com>
2021-06-03 21:05:17 +0800

    Create InterfaceInheritance_Inter.cs

---
+++ UnityCsharp/Amadeus/Pandora'Box/Test/InterfaceInheritance_Inter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NF.Test
+{
+    public interface IInterfaceInheritance_Inter
+    {
+        int X { get; set; }
+        int Y { get; set; }
+        void Halo();
+        void Vector(int vX, int vY);
+    }
+}
