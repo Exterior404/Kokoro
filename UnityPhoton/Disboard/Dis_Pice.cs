5c1d4c0dc83f5439ee02868b45160d675ef2bc85
NightFeather <reingeass@gmail.com>
2021-06-04 16:47:34 +0800

    Create Dis_Pice.cs

---
+++ UnityPhoton/Disboard/Dis_Pice.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dis_Pice : MonoBehaviour
+{
+    public int row;
+    public int column;
+    public PiceColor piceColor = PiceColor.黑;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
+
+public enum PiceColor : byte
+{
+    黑 = 0,
+    白 = 1
+}
