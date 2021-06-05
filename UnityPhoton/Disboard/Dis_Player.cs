e3fb6bc32cf38f525587206e7f4a76780db68290
NightFeather <reingeass@gmail.com>
2021-06-06 00:01:12 +0800

    Create Dis_Player.cs

---
+++ UnityPhoton/Disboard/Dis_Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Dis_Player : MonoBehaviour
+{
+    public Vector3 zeroPos;
+    public float cellWidth;
+    public PiceColor piceColor = PiceColor.黑;
+
+    private PhotonView pv;
+    void Start()
+    {
+        pv = this.GetComponent<PhotonView>();
+    }
+
+
+    void FixedUpdate()
+    {
+        //判斷
+        if (!pv.IsMine) return;
+
+        //滑鼠 左鍵 0 右1 滾輪2
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+}
