e4f0d7069312244bca07f50d6c02dc402a839cd2
NightFeather <reingeass@gmail.com>
2021-06-07 06:21:40 +0800

    Create InterfaceInheritance_Par.cs

---
+++ UnityCsharp/Amadeus/Pandora'Box/Test/InterfaceInheritance_Par.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NF.Test {
+    sealed partial class InterfaceInheritance_Par : IInterfaceInheritance_Inter {
+        public static InterfaceInheritance_Par _instance;
+        void Awake()
+        {
+            _instance = this;
+        }
+
+        public int X {
+            get;
+            set;
+        }
+
+        public int Y {//改了會錯
+            get; //{ return Y; }
+            set; //{ Y = value; }
+        }
+
+        public void Halo() {
+            Debug.Log("Halo");
+        }
+
+        public void Vector(int vX, int vY) {
+            X = vX;
+            Y = vY;
+            int sum = X + Y;
+            Debug.Log(string.Format("X:{0}\t Y:{1}\t SUM={2}",X,Y,sum));
+        }
+    }
+}
