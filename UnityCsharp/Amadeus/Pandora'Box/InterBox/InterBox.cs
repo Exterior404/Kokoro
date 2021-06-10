ce95189852cac2cc38c7edeb2fcc5daa53974d1f
NightFeather <reingeass@gmail.com>
2021-06-11 00:13:21 +0800

    Create InterBox.cs

---
+++ UnityCsharp/Amadeus/Pandora'Box/InterBox/InterBox.cs
@@ -0,0 +1,16 @@
+﻿namespace NF.Test {
+    public interface IBox {
+        
+    }
+
+    public interface IBoxA : IBox
+    {
+        void Hello();
+    }
+}
+
+public interface IBoxB : NF.Test.IBox
+{
+    int num { get; set; }
+    int id { get; set; }
+}
